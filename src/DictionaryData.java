61f46ce799b51d77f3a582f2b9b704cae6748128
Carles Sentis <carles@091labs>
2011-08-09 13:12:03 +0100

    Tue Aug 9 13:11

--- src/DictionaryData.java
+++ src/DictionaryData.java
@@ -1,45 +1,42 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class DictionaryData {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		TernarySearchTree tree = new TernarySearchTree(new TernaryTreeNode('A', false));
-		
+
 		try {
-		    BufferedReader in = new BufferedReader(new FileReader("./dic/dictionary.txt"));
-		    
-		    String str = null;
-		    
-		    while ((str = in.readLine()) != null) {
-		    	tree.insert(str.toLowerCase());
-		    }
-		    in.close();
-		    
+			BufferedReader in = new BufferedReader(new FileReader(
+					"./dic/dictionary.txt"));
+
+			String str = null;
+
+			while ((str = in.readLine()) != null) {
+				tree.insert(str.toLowerCase());
+			}
+			in.close();
+
 		} catch (IOException e) {
 			System.out.println("Something went wrong !!!");
 		}
-			
-		if (tree.containsKey("abella")) System.out.println("abella is found in the tree");
-		else System.out.println("abella is not found");
+
+		String string = "";
+		InputStreamReader input = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(input);
+
+		do{
+			System.out.print("Please enter a word: ");
+	
+			string = reader.readLine();
+	
+			if (tree.containsKey(string))
+				System.out.print("\n\t** " + string + " ** is found in the dictionary\n\n");
+			else
+				System.out.print("\n\t** " + string + " ** is NOT found in the dictionary\n\n");
 		
-		if (tree.containsKey("transport")) System.out.println("transport is found in the tree");
-		else System.out.println("transport is not found");
-		
-		if (tree.containsKey("zuau")) System.out.println("zuau is found in the tree");
-		else System.out.println("zuau is not found");
-		
-		if (tree.containsKey("creure")) System.out.println("creure is found in the tree");
-		else System.out.println("creure is not found");
-		
-		if (tree.containsKey("governar")) System.out.println("governar is found in the tree");
-		else System.out.println("governar is not found");
-		
-		if (tree.containsKey("carless")) System.out.println("carless is found in the tree");
-		else System.out.println("carless is not found");
-		
-		if (tree.containsKey("lliure")) System.out.println("lliure is found in the tree");
-		else System.out.println("lliure is not found");
+		}while (string != "123");
 	}
 }
--- src/WordSorting.java
+++ src/WordSorting.java
@@ -3,14 +3,16 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class WordSorting {
 
 	public static void main(String... aArgs) throws IOException {
+		HashMap<Integer, ArrayList<WordItem>> wordMap = new HashMap<Integer, ArrayList<WordItem>>();
 		
 		try {
 		    BufferedReader in = new BufferedReader(new FileReader("./dic/Catalan.txt"));
-		    BufferedWriter out = new BufferedWriter(new FileWriter("./dic/Catalan_clean.txt"));
 		    String str, repeats="ZZZZZZZZ";
 		    int removed = 0;
 		    
@@ -19,9 +21,18 @@ public class WordSorting {
 		    	String[] words = str.split (" ");
 		    	
 		    	if (words[0].length() > 1) {
-		    		if (!repeats.equals(words[0])) 
-		    			out.write(words[0]  + "\n");
-		    		else {
+		    		
+		    		if (!repeats.equals(words[0])) {
+		    			
+		    			ArrayList<WordItem> wordItemList;
+		    			
+		    			if (wordMap.containsKey(words[0].length())) 	wordItemList = wordMap.get(words[0].length());
+		    			else 											wordItemList = new ArrayList<WordItem>();
+		    			
+		    			wordItemList.add(new WordItem(words[0]));
+	    				wordMap.put(words[0].length(), wordItemList);
+		    			//out.write(start + words[0]  + end);
+		    		} else {
 		    			System.out.println("Removing --> Repeat: " + words[0]);
 		    			removed++;
 		    		}
@@ -33,9 +44,34 @@ public class WordSorting {
 		    }
 		    System.out.println("-------------\nTotal removed: " + removed);
 		    in.close();
-		    out.close();
 		} catch (IOException e) {
 			System.out.println("Something went wrong !!!");
 		}
+		
+		
+		BufferedWriter out = new BufferedWriter(new FileWriter("./dic/dic_catalan.xml"));
+		int n = 0;
+		int x;
+		do {
+			if (wordMap.containsKey(n)) {
+				
+				ArrayList<WordItem> wordItemList = wordMap.get(n);
+				out.write("\n\nWriting: " + n + "n\n");
+				x = 0;
+				
+				for (WordItem w : wordItemList) {
+					if (x == 10) {
+						out.write("\n");
+						x = 0;
+					}
+					x++;
+					out.write("\"" + w.getDisplayWord() + "|" + w.getSearchWord() + "\", ");
+				}
+				out.write("\n\n\n");
+			}
+			n++;
+		}while(n<30);
+		
+		out.close();
 	}
 }
